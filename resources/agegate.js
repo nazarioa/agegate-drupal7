d441f7902da7fb70d6e8aa4fcb6993f706afd264
Nazario A. Ayala <nazario@niztech.com>
2015-10-27 22:50:07 -0700

    Added TPL fucntioanlity and exposed text for buttons

--- resources/agegate.js
+++ resources/agegate.js
@@ -5,45 +5,59 @@
 function startAgegate(context, setting) {
   //settings brought in from drupal config
   var DEBUG         = Drupal.settings.agegate.debugmode;
+  var TPLHTML       = Drupal.settings.agegate.popuphtml;
   var COOKIE_DOMAIN = Drupal.settings.agegate.cookiedomain;
-  var SITENAME      = Drupal.settings.agegate.sitename;
-  var MESSAGE       = Drupal.settings.agegate.sitemessage;
-  var LOGOSHOW      = Drupal.settings.agegate.logoshow;
-  var LOGOURL       = Drupal.settings.agegate.logourl;
-  var BUTTONTEXT    = Drupal.settings.agegate.buttontext;
-  var DISPLAY    	  = Drupal.settings.agegate.display;
+  var COOKIE_NAME   = Drupal.settings.agegate.cookiename;
+  var COOKIE_FORMAT = Drupal.settings.agegate.cookieformat;
+  var VERIFY_BTN_TEXT = Drupal.settings.agegate.verifybtntext;
+  var CANCEL_BTN_TEXT = Drupal.settings.agegate.cancelbtntext;
+  var CANCEL_URL    = Drupal.settings.agegate.cancelurl;
+  var DISPLAY    	  = Drupal.settings.agegate.displaypopup;
   var LEGALAGE      = Drupal.settings.agegate.legalage;
 
-  jQuery.cookie.raw = false;
+  if(COOKIE_FORMAT === 1){
+    jQuery.cookie.raw = true;
+  }else{
+    jQuery.cookie.raw = false;
+  }
+
   var cookie_options = {
     "expires": 1,
     "path": '/',
     "domain" : COOKIE_DOMAIN
   }
-  var COOKIE_NAME = 'ISLEGAL';
+  // var COOKIE_NAME = 'ISLEGAL';
+  var agegateHtmlWrapper = '<div id="agegate" style="display:block"><div class="agegate-message">' + TPLHTML + '<div class="buttonBar"><button id="agegateCancel" class="button verify-age">' + CANCEL_BTN_TEXT + '</button><button id="agegateVerify" class="button verify-age">' + VERIFY_BTN_TEXT + '</button></div></div></div>';
 
-  var agegatehtml = '<div id="agegate" style="display:block"><div class="agegate-message">' + (LOGOSHOW == true ? '<img class="agegate-logo" src="' + LOGOURL + '" alt="Logo"/>' : '' ) + '<h1>Welcome to '+ SITENAME + '</h1><p>' + MESSAGE + '</p><span id="olderthan" class="button verify-age">' + BUTTONTEXT + '</span></div></div>';
-
-  var cookieValue = '{"ROWCOUNT":1,"COLUMNS":["ISLEGAL","REDIRECTURL"],"DATA":{"ISLEGAL":["1"],"REDIRECTURL":["http://www.centurycouncil.org/"]}}';
+  var cookieValue = '{"ROWCOUNT":1,"COLUMNS":["ISLEGAL","REDIRECTURL"],"DATA":{"ISLEGAL":["1"],"REDIRECTURL":["' + CANCEL_URL + '"]}}';
 
   if(jQuery.cookie(COOKIE_NAME) == null){
-    if(DEBUG === true) {
+    if(DEBUG === 1) {
+      console.log("Agegate: Cookie's name is: " + COOKIE_NAME);
       console.log("Agegate: Agegate / eWinery cookie not found");
       console.log("Agegate: Displaying Agegate");
     }
 
-    jQuery('body').append(agegatehtml);
+    jQuery('body').append(agegateHtmlWrapper);
   }
 
   // Register event listener for age gate submit button
-  jQuery('#olderthan').click(function() {
-    if(DEBUG === true) {
+  jQuery('#agegateVerify').click(function() {
+    if(DEBUG === 1) {
+      console.log("Agegate: user's age is: " + jQuery('#agegate-userage').val() );
       console.log("Agegate: User has confirmed legal age, setting cookie");
       console.log("Agegate: Hiding agegate");
     }
 
     jQuery.cookie(COOKIE_NAME, cookieValue, cookie_options);
     jQuery('#agegate').css('display','none');
+  });
+
+  jQuery('#agegateCancel').click(function() {
+    if(DEBUG === 1) {
+      console.log("Agegate: User has canceled.");
+    }
+    window.location = CANCEL_URL;
   });
 }
 
@@ -52,7 +66,7 @@ function startAgegate(context, setting) {
 */
 Drupal.behaviors.agegate = {
   attach: function (context, settings) {
-    if(Drupal.settings.agegate.display === true){
+    if(Drupal.settings.agegate.displaypopup === true){
       startAgegate(context, settings);
     }
   }
