c67580487c6ce4557b5eccce7f2df4812b26c0c8
Nazario A. Ayala <nazario@niztech.com>
2015-10-31 02:30:53 -0700

    Several Changes - Removed Logo - Added ability to dictate redirect - Changed logic to use YES/NO HIDDEN/SHOW constants - Added TPL feature - Documented validation - Removed extraneous hooks

---
+++ agegate--popup.tpl.php
@@ -0,0 +1,11 @@
+<div class="title">
+  <?php echo $agegate['name']; ?>
+</h1>
+
+<?php echo $agegate['message']; ?>
+<?php echo $agegate['validate_box']; ?>
+
+<div class="button-bar">
+  <?php echo $agegate['cancel_button']; ?>
+  <?php echo $agegate['verify_button']; ?>
+</div>
