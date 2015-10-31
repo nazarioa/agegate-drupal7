2a3abc08f8619a9b9265a1b3bfe625cc2314f788
Nazario A. Ayala <nazario@niztech.com>
2015-10-31 16:29:14 -0700

    Added comments to tpl file

--- agegate--popup.tpl.php
+++ agegate--popup.tpl.php
@@ -1,11 +1,13 @@
+<!-- start agegate__popup.tpl.php -->
 <div class="title">
-  <?php echo $agegate['name']; ?>
+  <?php echo $agegate['name']; ?> <!-- title - not-required -->
 </div>
 
-<?php echo $agegate['message']; ?>
-<?php echo $agegate['validate_box']; ?>
+<?php echo $agegate['message']; ?> <!-- legal message - not-required -->
+<?php echo $agegate['validate_box']; ?> <!-- validation button code - REQUIRED -->
 
 <div class="button-bar">
-  <?php echo $agegate['cancel_button']; ?>
-  <?php echo $agegate['verify_button']; ?>
+  <?php echo $agegate['cancel_button']; ?> <!-- cancel button code - not required -->
+  <?php echo $agegate['verify_button']; ?> <!-- verify button code - REQUIRED -->
 </div>
+<!-- end agegate__popup.tpl.php -->
