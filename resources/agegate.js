d4071b5e18501e38a0f23a4e1626627387839667
Nazario A. Ayala <nazario@niztech.com>
2015-11-01 22:33:00 -0800

    Added Three features - Added TPL file support - Added age validation based on validate - Added ability to not display Age Gate for some pages

--- resources/agegate.js
+++ resources/agegate.js
@@ -5,6 +5,7 @@
 function startAgegate(context, setting) {
   //settings brought in from drupal config
   var DEBUG         = Drupal.settings.agegate.debugmode;
+  var VERIFICATIONTYPE = Drupal.settings.agegate.verifcationtype;
   var TPLHTML       = Drupal.settings.agegate.popuphtml;
   var COOKIE_DOMAIN = Drupal.settings.agegate.cookiedomain;
   var COOKIE_NAME   = Drupal.settings.agegate.cookiename;
@@ -24,39 +25,62 @@ function startAgegate(context, setting) {
     "path": '/',
     "domain" : COOKIE_DOMAIN
   }
-  // var COOKIE_NAME = 'ISLEGAL';
-  var agegateHtmlWrapper = '<div id="agegate" style="display:block"><div class="agegate-message">' + TPLHTML + '</div></div>';
 
+  var agegateHtmlWrapper = '<div id="agegate" style="display:block"><div class="agegate-message">' + TPLHTML + '</div></div>';
   var cookieValue = '{"ROWCOUNT":1,"COLUMNS":["ISLEGAL","REDIRECTURL"],"DATA":{"ISLEGAL":["1"],"REDIRECTURL":["' + CANCEL_URL + '"]}}';
 
+  // Check for presence of the cookie, if none, append the HTML file.
   if(jQuery.cookie(COOKIE_NAME) == null){
     if(DEBUG === 1) {
       console.log("Agegate: Cookie's name is: " + COOKIE_NAME);
       console.log("Agegate: Agegate / eWinery cookie not found");
       console.log("Agegate: Displaying Agegate");
     }
-
     jQuery('body').append(agegateHtmlWrapper);
   }
 
   // Register event listener for age gate submit button
   jQuery('#agegate_verify').click(function() {
-    if(DEBUG === 1) {
-      console.log("Agegate: user's age is: " + jQuery('#agegate-userage').val() );
-      console.log("Agegate: User has confirmed legal age, setting cookie");
-      console.log("Agegate: Hiding agegate");
+    if( validate() === true ){
+      if(DEBUG === 1) {
+        console.log("Agegate: user's age is: " + jQuery('#agegate-userage').val() );
+        console.log("Agegate: User has confirmed legal age, setting cookie");
+        console.log("Agegate: Hiding agegate");
+      }
+      jQuery.cookie(COOKIE_NAME, cookieValue, cookie_options);
+      jQuery('#agegate').css('display','none');
+    }else{
+      if(DEBUG === 1) {
+        console.log("Agegate: did not validate.");
+      }
     }
-
-    jQuery.cookie(COOKIE_NAME, cookieValue, cookie_options);
-    jQuery('#agegate').css('display','none');
   });
 
+  // Register event listener for age gate cancel button
   jQuery('#agegate_cancel').click(function() {
     if(DEBUG === 1) {
       console.log("Agegate: User has canceled.");
     }
     window.location = CANCEL_URL;
   });
+
+  // Age Validation Logic
+  function validate(){
+    if(VERIFICATIONTYPE == 1){
+      var cutoff = new Date();
+      cutoff.setFullYear(cutoff.getFullYear() - LEGALAGE );
+      var birthday = new Date( jQuery('#agegate_birthday').val() );
+      var result = cutoff - birthday;
+      if(result > 0){
+        return true;
+      }else{
+        return false;
+      }
+
+    }else if(VERIFICATIONTYPE == 2){
+      return true;
+    }
+  }
 }
 
 /**
