022dd706fe6a159ca3c407ab48846e06dbf49f75
Nazario A. Ayala <nazario@niztech.com>
2015-11-06 20:38:57 -0800

    added documentation to tpl.php file

--- agegate--popup.tpl.php
+++ agegate--popup.tpl.php
@@ -1,3 +1,21 @@
+/**
+ * @file
+ * Displays the Agegate.
+ *
+ * Available variables:
+ * - $agegate (array): Contains all the variables avilable to the Agegare
+ *   - $agegate['name']: Title set in the settings.
+ *   - $agegate['message']: The message set in the settings.
+ *   - $agegate['validate_box']: REQUIRED - Contains the code. This changes
+ *     depending on the validation type.
+ *   - $agegate['cancel_button']: Code for a cancel button if you need.
+ *   - $agegate['verify_button']: REQUIRED - button needed to triger the check.
+ *
+ * @see template_preprocess_forum_list()
+ *
+ * @ingroup themeable
+ */
+
 <!-- start agegate__popup.tpl.php -->
 <div class="title">
   <?php echo $agegate['name']; ?> <!-- title - not-required -->
