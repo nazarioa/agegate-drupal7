865b7c796ccf168b13af4481aefd9c84d7c09e3d
Nazario A. Ayala <nazario@niztech.com>
2015-11-08 12:22:33 -0800

    added document comments

--- resources/agegate.js
+++ resources/agegate.js
@@ -16,8 +16,14 @@
 
       /**
        * AgeGate function.
+       *
+       * @param {string} context - The DOM as sent by drupal?
+       * @param {string} setting - An array with settings used by
+       *  sent by hook_preprocess_page.
        */
+
       function startAgegate(context, setting) {
+        console.log(context);
 
         // Settings brought in from drupal config.
         var VERIFICATIONTYPE = Drupal.settings.agegate.verifcationtype;
@@ -39,8 +45,7 @@
         $('#agegate_verify').click(function () {
           if (validate() === true) {
             var cookieValue = '{"ROWCOUNT": 1, "COLUMNS": ["ISLEGAL", "REDIRECTURL"], "DATA":{"ISLEGAL": ["1"], "REDIRECTURL": ["' + CANCEL_URL + '"]}}';
-            console.log(COOKIE_FORMAT);
-            if(COOKIE_FORMAT === '1'){
+            if (COOKIE_FORMAT === '1') {
               cookieValue = escape(cookieValue);
             }
 
